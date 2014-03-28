2de0792d266aa722abe8bb2f1079bf988cf1dcfe
saidgeek <atarix.010101@gmail.com>
2014-03-28 17:17:51 -0300

    cambio en gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -462,7 +462,7 @@ module.exports = function (grunt) {
     if (target === 'debug') {
       return grunt.task.run([
         'clean:server',
-        'bower-install',
+        // 'bower-install',
         'concurrent:server',
         'autoprefixer',
         'concurrent:debug'
@@ -471,7 +471,7 @@ module.exports = function (grunt) {
 
     grunt.task.run([
       'clean:server',
-      'bower-install',
+      // 'bower-install',
       'concurrent:server',
       'autoprefixer',
       'express:dev',
